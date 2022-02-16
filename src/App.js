3cbe3c34c90f795df9cb9aa03dd276872eb1e56f
seifeddine bourkhis <seifeddinebourkhis11@gmail.com>
2022-02-16 13:36:18 +0100

    prorea

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import SimpleForm from "./BigForm/SimpleForm";
+
+
+function App() {
+  return (
+    <div className="App">
+      <SimpleForm/>
+    </div>
+  );
+}
+
+export default App;
